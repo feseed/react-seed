1dcce8a3b3d7a1dc487ad16a0cc173a282648536
gauseen <gauseen@163.com>
2018-05-14 19:11:13 +0800

    update hashRouter、README.md

---
+++ app-seed-pc/config-overrides.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: gauseen 
+ * @Date: 2018-05-14 18:15:21 
+ * @Last Modified by: gauseen
+ * @Last Modified time: 2018-05-14 18:22:33
+ * @覆盖 create-react-app webpack 默认配置
+ */
+module.exports = {
+	webpack: function(config, env) {
+		return config
+	},
+}
--- app-seed-pc/src/layout/Layout.jsx
+++ app-seed-pc/src/layout/Layout.jsx
@@ -14,7 +14,6 @@ class CLayout extends Component {
 					<RouterDom/>
 				</MainContent>
 			</Layout>
-			
 		)
 	}
 }
